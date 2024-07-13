51a873ec49a34e2c0d8460948811ab1836a7cb91
thomasatwoodrobinson <thomasatwoodrobinson@gmail.com>
2024-07-12 23:10:07 -0400

    added decryption module

--- controller.py
+++ controller.py
@@ -17,33 +17,48 @@ class controller():
             if list[i] == letter:
                 return i 
             
-
+    def search_for_letter(self, list, letter, table):
+        print(list)
+        for i in range(len(list)-1):
+            print("index: " + str(i) + " looking for: " + letter)
+            if list[i] == letter:
+                return table[i][0]
 
     def encryption(self, plaintext, secret_word, table, keyed_list):
         cyphertext = ""
-        for i in range(len(plaintext)-1):
-            secret_word_index = self.search(keyed_list, secret_word[i%7])
-            print(secret_word_index)
+        for i in range(len(plaintext)):
+            secret_word_index = self.search(keyed_list, secret_word[i%(len(secret_word))])
             plaintext_index = self.search(keyed_list, plaintext[i])
             cyphertext = cyphertext + table[secret_word_index][plaintext_index] + " "
         return cyphertext
-
-
-
+    
+    def decryption(self, cyphertext, secret_word, table, keyed_list):
+        plaintext = ""
+        for i in range(len(cyphertext)):
+            keyed_list_index = self.search(keyed_list, secret_word[i%(len(secret_word))])
+            plaintext = plaintext + self.search_for_letter(table[keyed_list_index], cyphertext[i], table) + " "
+        return plaintext
 
     def main(self):
         secret_word = []
-        PT = input_handling.plaintext()
+        PT = input_handling.text()
         VT = viginere.viginere_table()
-        plaintext = PT.get_plaintext()
-        print(plaintext)
+        CT = input_handling.text()
         secret_word = self.secretWord()
-        print(secret_word)
         keyed_list = VT.keyed_list()
-        print(keyed_list)
         table = VT.matrix(keyed_list)
-        print(table)
-        print (self.encryption(plaintext, secret_word, table, keyed_list))
+        i = 0
+        while i < 1:
+            print('Encryption or Decryption (e/d)? ')
+            answer = input().lower()
+            if answer == 'e':
+                i = i+1
+                plaintext = PT.get_plaintext()
+                print (self.encryption(plaintext, secret_word, table, keyed_list))
+            elif answer == 'd':
+                i = i+1
+                cyphertext = CT.get_cyphertext()
+                print (self.decryption(cyphertext, secret_word, table, keyed_list))
         return 0
     
 run = controller()
--- input_handling.py
+++ input_handling.py
@@ -1,4 +1,4 @@
-class plaintext():
+class text():
 
     def remove_spaces(self, word):
         return word.replace(" ", "")
@@ -11,5 +11,14 @@ class plaintext():
         for letter in plaintext_smush:
             plaintext.append(letter)
         return plaintext
+    
+    def get_cyphertext(self):
+        cyphertext = []
+        print("Enter cyphertext")
+        cyphertext_initial = input().lower()
+        cyphertext_smush = self.remove_spaces(cyphertext_initial)
+        for letter in cyphertext_smush:
+            cyphertext.append(letter)
+        return cyphertext
     
     
